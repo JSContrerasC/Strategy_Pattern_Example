5dd530c04071d552902546578eaec52019615fb4
JSContreras <joaquincelmaperdigon@hotmail.com>
2020-02-05 10:13:18 -0300

    Initial commit

---
+++ src/main/java/ShoppingCartTest.java
@@ -0,0 +1,17 @@
+public class ShoppingCartTest {
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+
+        Item item1 = new Item("1234",10);
+        Item item2 = new Item("5678",40);
+
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        //pay by paypal
+        cart.pay(new PayPalStrategy("correo", "mypwd"));
+
+        //pay by credit card
+        cart.pay(new CreditCardStrategy("Javi Pache", "1234567890123456", "000", "12/25"));
+    }
+}
